511c9d33a923f25d6fb027e079c6574f5551a0ac
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-12-13 09:54:47 +0530

    Update start.py

--- SONALI/utils/inline/start.py
+++ SONALI/utils/inline/start.py
@@ -25,7 +25,7 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_5"], user_id="5446367898"),
             InlineKeyboardButton(text="˹ ᴧʙᴏᴜᴛ ˼", callback_data="gibt_source"),
         ],
 [InlineKeyboardButton(text=_["S_B_4"], callback_data="gib_source")],
